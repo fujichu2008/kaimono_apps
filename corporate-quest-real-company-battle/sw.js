6a7e49e5f423e85657e6732722115c7aeb933b53
fujichu2008 <hs2004.0134@gmail.com>
2026-07-19 21:57:51 +0900

    Publish corporate-quest-real-company-battle PWA via AppStudioMobile

--- corporate-quest-real-company-battle/sw.js
+++ corporate-quest-real-company-battle/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-corporate-quest-real-company-battle-9f39295c937b3d2dd083";
+const CACHE_NAME = "appstudio-pwa-corporate-quest-real-company-battle-e19246ce19f21bcccc4f";
 const CACHE_PREFIX = "appstudio-pwa-corporate-quest-real-company-battle-";
 const PRECACHE_URLS = [
     "./",
