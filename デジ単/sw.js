40c1e2306f93127102c778098c5fe03e70bf0b45
fujichu2008 <hs2004.0134@gmail.com>
2026-07-25 18:18:58 +0900

    Publish デジ単 PWA via AppStudioMobile

--- デジ単/sw.js
+++ デジ単/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-デジ単-d874bc830880cc5a0763";
+const CACHE_NAME = "appstudio-pwa-デジ単-e3f4916179db711414e8";
 const CACHE_PREFIX = "appstudio-pwa-デジ単-";
 const PRECACHE_URLS = [
     "./",
