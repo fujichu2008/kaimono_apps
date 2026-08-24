0c7b1bcb16eb2fc68af485389768bdc611b94c04
fujichu2008 <hs2004.0134@gmail.com>
2026-08-25 06:06:31 +0900

    Publish mamサポーター PWA via AppStudioMobile

--- mamサポーター/sw.js
+++ mamサポーター/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-mamサポーター-55b286032e845f430084";
+const CACHE_NAME = "appstudio-pwa-mamサポーター-fe2714a199886ed362a1";
 const CACHE_PREFIX = "appstudio-pwa-mamサポーター-";
 const PRECACHE_URLS = [
     "./",
