59ba6c6d580f64f245054fed32286fc07726288c
fujichu2008 <hs2004.0134@gmail.com>
2026-09-05 12:56:38 +0900

    Publish speaknote-英語例文-場面学習 PWA via AppStudioMobile

--- speaknote-英語例文-場面学習/sw.js
+++ speaknote-英語例文-場面学習/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-speaknote-英語例文-場面学習-89433d90b2033d6b2790";
+const CACHE_NAME = "appstudio-pwa-speaknote-英語例文-場面学習-56320adc130b2870003f";
 const CACHE_PREFIX = "appstudio-pwa-speaknote-英語例文-場面学習-";
 const PRECACHE_URLS = [
     "./",
