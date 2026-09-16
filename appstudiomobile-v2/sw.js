faac27f182680759fbb7440eb7214ad204c74f7a
fujichu2008 <hs2004.0134@gmail.com>
2026-09-17 07:50:41 +0900

    Publish appstudiomobile-v2 PWA via AppStudioMobile

--- appstudiomobile-v2/sw.js
+++ appstudiomobile-v2/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-appstudiomobile-v2-33751684722c091c55c9";
+const CACHE_NAME = "appstudio-pwa-appstudiomobile-v2-5a95c3a3f2e26edf6eba";
 const CACHE_PREFIX = "appstudio-pwa-appstudiomobile-v2-";
 const PRECACHE_URLS = [
     "./",
