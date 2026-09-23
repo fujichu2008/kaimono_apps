de30f3c429c1e229c1362ec6b1b8d542c224537b
fujichu2008 <hs2004.0134@gmail.com>
2026-09-23 21:36:30 +0900

    Publish かりうち PWA via AppStudioMobile

--- かりうち/sw.js
+++ かりうち/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "appstudio-pwa-かりうち-9fe2f7d8ce7f2df893e3";
+const CACHE_NAME = "appstudio-pwa-かりうち-57f7329992efcb73c054";
 const CACHE_PREFIX = "appstudio-pwa-かりうち-";
 const PRECACHE_URLS = [
     "./",
